11e8e50c619e319662bf9c76ee4d267ce6e0c0b8
JrSchild <joriami@gmail.com>
2014-05-29 15:46:02 +0200

    import buildscript

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+/*
+ * grunt-contrib-sass
+ * http://gruntjs.com/
+ *
+ * Copyright (c) 2012 Sindre Sorhus, contributors
+ * Licensed under the MIT license.
+ */
+'use strict';
+
+module.exports = function (grunt) {
+  grunt.initConfig({
+    jshint: {
+      options: {
+        node: true
+      },
+      all: [
+        'Gruntfile.js',
+        'dist/jr-crop.js'
+      ]
+    },
+    clean: {
+      build: ['.sass-cache']
+    },
+    sass: {
+      dist: {
+        files: {
+          'dist/jr-crop.css': 'dist/jr-crop.scss'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  grunt.registerTask('default', ['jshint']);
+};
