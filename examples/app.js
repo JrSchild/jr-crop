703837f8ebf5eb77bee36180fe89464f5442532b
JrSchild <joram.ruitenschild@gmail.com>
2015-11-16 10:01:36 +0100

    Update examples on how to upload the file as BLOB and store them on the server.

--- examples/app.js
+++ examples/app.js
@@ -1,18 +1,45 @@
-var static = require('node-static');
+var fs = require('fs');
+var path = require('path');
+var express = require('express');
+var formidable = require('formidable');
+var app = express();
 
-var r_match_vendor = /^\/(?:bower|dist)/;
+app.use(express.static('./'));
+app.use(express.static('./examples'));
 
-var vendor = new static.Server('./');
-var examples = new static.Server('./examples');
+var types = {
+  'image/jpeg': 'jpg',
+  'image/png': 'png'
+};
 
-require('http').createServer(function (req, res) {
-  req.addListener('end', function () {
-    if (r_match_vendor.test(req.url)) {
-      return vendor.serve(req, res);
+// Upload the file to memory.
+app.post('/upload', function (req, res) {
+
+  // Parse the request. Any body-parser can be used for this.
+  var form = new formidable.IncomingForm();
+  form.parse(req, function(err, fields, files) {
+
+    // The key was set as 'image' on the formData object in the client.
+    var file = files.image;
+
+    var extension = types[file.type.toLowerCase()];
+
+    // Make sure the extension is valid.
+    if (!extension) {
+      return res.status(400).send('Invalid file type');
     }
 
-    examples.serve(req, res);
-  }).resume();
-}).listen(8181);
+    // Target path is uploads directory relative to this file.
+    var target = path.resolve(__dirname, 'uploads', Date.now() + '.' + extension);
 
-console.log('Running jr-crop example server. Visit http://localhost:8181');
+    // Copy the file from the tmp folder to the uploads location
+    fs.createReadStream(file.path)
+      .pipe(fs.createWriteStream(target));
+
+    res.status(200).send();
+  });
+});
+
+var server = app.listen(8181, function () {
+  console.log('Running jr-crop example server. Visit http://localhost:8181');
+});
